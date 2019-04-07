28e513bb00dd1d70ec75ae28fdb2496f763685c0
Japaness <49285807+Japaness@users.noreply.github.com>
2019-04-07 14:40:03 +0200

    Wrong user

--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-var Discord = require('discord.js');
-exports.method = function(client, channel, color, footer){
-    var pingEmbed = new Discord.RichEmbed()
-    .addField('Current latency :', Math.round(client.ping, 2) + ' milliseconds.')
-    .setColor(color)
-    .setFooter(footer)
-    channel.send(pingEmbed);
-}
